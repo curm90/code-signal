2f46caff9b2ff4b8c4510793ff34794ad75beab5
Liam Sutton <devliam90@gmail.com>
2020-07-23 14:09:05 +0100

    update solution add

--- arcade/intro/add.js
+++ arcade/intro/add.js
@@ -1,3 +1,11 @@
 function add(param1, param2) {
-  return param1 + param2
+  return param1 + param2;
 }
+
+const add = (param1, param2) => {
+  return param1 + param2;
+};
+
+const add = (param1, param2) => param1 + param2;
+
+console.log(add(3, 5));
