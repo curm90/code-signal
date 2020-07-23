b478d839336311821fb3f2c97bef74d9b7085803
Liam Sutton <devliam90@gmail.com>
2020-07-23 14:40:00 +0100

    check palindrome additional

--- arcade/intro/century-from-year.js
+++ arcade/intro/century-from-year.js
@@ -1,21 +1,27 @@
 function centuryFromYear(year) {
-  const century = Math.floor(year / 100)
-  const decade = year % 100
-  
+  const century = Math.floor(year / 100);
+  const decade = year % 100;
+
   if (decade > 0) {
-    return century + 1
+    return century + 1;
   } else {
-    return century
+    return century;
   }
 }
 
 function centuryFromYear(year) {
-  return Math.ceil(year / 100)
+  const century = Math.floor(year / 100);
+  const decade = year % 100;
+
+  return decade > 0 ? century + 1 : century;
 }
 
 function centuryFromYear(year) {
-  return Math.floor((year-1) / 100) + 1;
+  return Math.ceil(year / 100);
 }
 
+function centuryFromYear(year) {
+  return Math.floor((year - 1) / 100) + 1;
+}
 
 console.log(centuryFromYear(1901));
--- arcade/intro/check-palindrome.js
+++ arcade/intro/check-palindrome.js
@@ -1,12 +1,39 @@
 function checkPalindrome(str) {
-    if (str.length === 1) return true
-    const reverse = str.split('').reverse().join('')
-    return reverse === str
+  if (str.length === 1) return true;
+  const reverse = str.split('').reverse().join('');
+  return reverse === str;
 }
 
-function checkPalindrome(inputString) {
-    return inputString == inputString.split('').reverse().join('');
+function checkPalindrome(str) {
+  return str == str.split('').reverse().join('');
 }
 
+const checkPalindrome = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    const letter = str[i];
 
-console.log(checkPalindrome('ddgdd'))
+    if (letter !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const checkPalindrome = (str) => {
+  const charArr = str.split('');
+  for (let letter of charArr) {
+    if (letter !== charArr.pop()) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const checkPalindrome = (str) => {
+  return str
+    .split('')
+    .every((letter, i) => letter === str[str.length - 1 - i]);
+};
+
+console.log(checkPalindrome('ddgdd'));
+console.log(checkPalindrome('ddgddd'));
