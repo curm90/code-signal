0c20f9543c3c42cf10fab94af7802a4fbd55173c
Liam Sutton <devliam90@gmail.com>
2020-07-23 15:05:37 +0100

    adjacent element for loop

--- arcade/intro/adjacent-elements.js
+++ arcade/intro/adjacent-elements.js
@@ -1,20 +1,38 @@
 function adjacentElementsProduct(arr) {
-  return arr.slice(0, -1)
-  .reduce((acc, current, i) => Math.max(acc, current * arr[i + 1]), -Infinity)
+  return arr
+    .slice(0, -1)
+    .reduce(
+      (acc, current, i) => Math.max(acc, current * arr[i + 1]),
+      -Infinity
+    );
 }
 
 function adjacentElementsProduct(arr) {
-  return Math.max(...arr.slice(1).map((x, i) => [x * arr[i]]))
+  return Math.max(...arr.slice(1).map((x, i) => [x * arr[i]]));
 }
 
 function adjacentElementsProduct(inputArray) {
-  const prod = inputArray[0] * inputArray[1];
-  
+  let prod = inputArray[0] * inputArray[1];
+
   for (let i = 1; i < inputArray.length - 1; i++) {
     prod = Math.max(prod, inputArray[i] * inputArray[i + 1]);
   }
-  return prod
+  return prod;
 }
 
+const adjacentElementsProduct = (arr) => {
+  let result = -Infinity;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    const num = arr[i];
+
+    const sum = num * arr[i + 1];
+
+    if (sum > result) {
+      result = sum;
+    }
+  }
+  return result;
+};
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
