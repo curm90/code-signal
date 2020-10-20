ed20230bf5df37115c5c86c70b2884d59450316a
Liam Sutton <devliam90@gmail.com>
2020-10-20 12:32:47 +0100

    quicksort

---
+++ interview-practice/sorting/quicksort.js
@@ -0,0 +1,32 @@
+//app.codesignal.com/interview-practice/task/pLfjBLu9QwuSX47Cv/description
+
+https: function quickSort(a, l, r) {
+  if (l >= r) {
+    return a;
+  }
+
+  var x = a[l];
+  var i = l;
+  var j = r;
+
+  while (i <= j) {
+    while (a[i] < x) {
+      i++;
+    }
+    while (a[j] > x) {
+      j--;
+    }
+    if (i <= j) {
+      var t = a[i];
+      a[i] = a[j];
+      a[j] = t;
+      i++;
+      j--;
+    }
+  }
+
+  quickSort(a, l, j);
+  quickSort(a, i, r);
+
+  return a;
+}
