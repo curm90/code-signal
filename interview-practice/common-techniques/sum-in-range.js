586893c9ead662581c49f241dfe0a09b3e45f418
Liam Sutton <devliam90@gmail.com>
2021-01-11 14:13:27 +0000

    best solutions i found. mine was too slow

--- interview-practice/common-techniques/sum-in-range.js
+++ interview-practice/common-techniques/sum-in-range.js
@@ -20,6 +20,41 @@ function sumInRange(nums, queries) {
   return (sum + mod) % mod;
 }
 
+function sumInRange(nums, queries) {
+  let partialSum = 0;
+  const mod = 1e9 + 7;
+  const prefixSums = nums.map((num) => (partialSum += num));
+
+  return queries.reduce((total, query) => {
+    total += prefixSums[query[1]] - (prefixSums[query[0] - 1] || 0);
+    return total < 0 ? total + mod : total % mod;
+  }, 0);
+}
+
+function sumInRange(nums, queries) {
+  const frequencyDeltas = Array(nums.length + 1).fill(0); // Create `frequencyDeltas` array to represent delta of each element from last attempt
+  let frequency = 0;
+  let total = 0;
+  let mod = 1000000007; // 10^9 + 7
+  let i;
+
+  frequencyDeltas;
+  // For each range in `queries`
+  for (i = 0; i < queries.length; i++) {
+    frequencyDeltas[queries[i][0]]++; // Add 1 to the index of `frequencyDeltas` of the start of the range
+    frequencyDeltas[queries[i][1] + 1]--; // Subtract 1 from the index of `frequencyDeltas` of the end of the range
+  }
+  frequencyDeltas;
+
+  // For each delta in `frequencyDeltas`
+  for (i = 0; i < nums.length; i++) {
+    frequency += frequencyDeltas[i]; // Add the delta to a running "delta total" that infers the frequency
+    total += nums[i] * frequency; // Add to the total: the corresponding value of `nums` multiplied by this frequency
+  }
+
+  return ((total % mod) + mod) % mod;
+}
+
 console.log(
   sumInRange(
     [3, 0, -2, 6, -3, 2],
